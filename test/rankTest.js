de2cf80823a1d999b0a0c75c41138da34f6c9708
LIANGFR2 <372334264@qq.com>
2020-08-27 19:35:24 +0800

    [homework][front] test: new test should return B when rating given voyage and history

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,10 +1,27 @@
 const rankTest = require('ava');
+const {rating} = require('../src/rank')
 
-rankTest('foo', t => {
-  t.pass();
-});
-
-rankTest('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
+rankTest('should return B when rating given voyage and history', t => {
+  let voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  let history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+  let myRating = rating(voyage, history);
+  t.is(myRating, 'B');
 });
