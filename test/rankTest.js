9ad088fd4577e2e80f9119a9b98bd26d79950844
LIANGFR2 <372334264@qq.com>
2020-08-27 19:51:45 +0800

    [homework][front] test: should return A when voyageRisk given voyage length is 10 and zone is china and history length is 18 and history has china zone

--- test/rankTest.js
+++ test/rankTest.js
@@ -25,3 +25,78 @@ rankTest('should return B when rating given voyage and history', t => {
   let myRating = rating(voyage, history);
   t.is(myRating, 'B');
 });
+
+rankTest('should return A \
+when voyageRisk \
+given voyage length is 10 and zone is china \
+and history length is 18 and history has china zone', t => {
+  //given
+  let voyage = {
+    zone: 'china',
+    length: 10,
+  };
+
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+    {
+      zone: 'east-indies1',
+      profit: 5,
+    },{
+      zone: 'west-indies2',
+      profit: 15,
+    },{
+      zone: 'china3',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa4',
+      profit: 7,
+    },
+    {
+      zone: 'east-indies5',
+      profit: 5,
+    },{
+      zone: 'west-indies6',
+      profit: 15,
+    },{
+      zone: 'china7',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa8',
+      profit: 7,
+    },
+    {
+      zone: 'east-indies9',
+      profit: 5,
+    },{
+      zone: 'west-indies10',
+      profit: 15,
+    },{
+      zone: 'china11',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa12',
+      profit: 7,
+    },
+  ];
+  // vpf=7 vr=9 chr=3
+  //when
+  let myRating = rating(voyage, history);
+
+  t.is(myRating, 'A');
+});
