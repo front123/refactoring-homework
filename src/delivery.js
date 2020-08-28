8227816aeb08fd600c1946f13c1983f794d4dcf3
LIANGFR2 <372334264@qq.com>
2020-08-28 21:04:27 +0800

    [homework][front] refactor: extract deliveryTime variables

--- src/delivery.js
+++ src/delivery.js
@@ -1,6 +1,6 @@
 function deliveryDate (anOrder, isRush) {
+  let deliveryTime;
   if (isRush) {
-    let deliveryTime;
     if ([
       'MA',
       'CT',
@@ -19,7 +19,6 @@ function deliveryDate (anOrder, isRush) {
     return anOrder.placedOn.plusDays(1 + deliveryTime);
   }
   else {
-    let deliveryTime;
     if ([
       'MA',
       'CT',
