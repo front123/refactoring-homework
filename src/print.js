81d77dc64cd6f5e768b11de74196ff9d4dec8143
LIANGFR2 <372334264@qq.com>
2020-08-28 20:04:45 +0800

    [homework][front] refactor: format code

--- src/print.js
+++ src/print.js
@@ -1,4 +1,4 @@
-function calculateOutstanding(borderSpacing){
+function calculateOutstanding(borderSpacing) {
   let totalAmount = 0;
   for (const o of borderSpacing) {
     totalAmount += o.amount;
@@ -6,14 +6,14 @@ function calculateOutstanding(borderSpacing){
   return totalAmount;
 }
 
-function getDueDate(){
+function getDueDate() {
   const today = new Date();
   return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 }
 
-function generateDetail(invoice, outstanding){
+function generateDetail(invoice, outstanding) {
   return `***********************\n**** Customer Owes ****\n***********************\n` +
-      `name: ${invoice.customer}\namount: ${outstanding}\namount: ${invoice.dueDate.toLocaleDateString()}`;
+    `name: ${invoice.customer}\namount: ${outstanding}\namount: ${invoice.dueDate.toLocaleDateString()}`;
 }
 
 function printOwing(invoice) {
